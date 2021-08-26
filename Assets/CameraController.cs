0e9231da44ca4a59632978c1c630c054872c52da
valentin-99 <61514366+valentin-99@users.noreply.github.com>
2021-08-26 17:58:55 +0300

    update player states

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -6,13 +6,6 @@ public class CameraController : MonoBehaviour
 {
     public Transform player;
 
-    // Start is called before the first frame update
-    /*void Start()
-    {
-        
-    }*/
-
-    // Update is called once per frame
     void Update()
     {
         transform.position = new Vector3(player.position.x, player.position.y, player.position.z);
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,32 +4,83 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody2D rb;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private enum State {idle, run, jump, fall_jump}
+    private State state = State.idle;
+    private Collider2D coll;
+    [SerializeField] private LayerMask ground;
 
-    // Start is called before the first frame update
-    /*void Start()
+
+    private void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        coll = GetComponent<Collider2D>();
+    }
 
-    }*/
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (Input.GetKey(KeyCode.A))
+        // Left
+        if (Input.GetAxis("Horizontal") < 0)
         {
             rb.velocity = new Vector2(-5, rb.velocity.y);
             transform.localScale = new Vector2(-1, 1);
         }
 
-        if (Input.GetKey(KeyCode.D))
+        // Right
+        else if (Input.GetAxis("Horizontal") > 0)
         {
             rb.velocity = new Vector2(5, rb.velocity.y);
             transform.localScale = new Vector2(1, 1);
         }
 
-        if (Input.GetKey(KeyCode.W))
+        else
+        {
+        }
+
+        // Jump
+        if ((Input.GetAxis("Vertical") > 0) && coll.IsTouchingLayers(ground))
         {
             rb.velocity = new Vector2(rb.velocity.x, 7.5f);
+            state = State.jump;
+        }
+
+        // Switch states
+        VelocityState();
+        anim.SetInteger("state", (int)state);
+    }
+
+    private void VelocityState()
+    {
+        // If player jumps he starts falling to the ground
+        if (state == State.jump)
+        {   
+            if (rb.velocity.y < .1f)
+            {
+                state = State.fall_jump;
+            }   
+        }
+
+        // If player is falling to the ground and he's touching it then switch to idle
+        else if (state == State.fall_jump)
+        {
+            if (coll.IsTouchingLayers(ground))
+            {
+                state = State.idle;
+            }
+        }
+
+        // If player is walking
+        else if (Mathf.Abs(rb.velocity.x) > 3)
+        {
+            state = State.run;
+        }
+
+        else
+        {
+            state = State.idle;
         }
     }
+
 }
